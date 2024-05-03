d66f2fefec73525a10df456f5dcf503d15ed7599
nandakharismas22 <126435181+nandakharismas22@users.noreply.github.com>
2024-05-03 11:38:48 +0700

    StateManagement

---
+++ flutter_praktikum_state1/android/app/src/main/kotlin/com/example/flutter_praktikum_state1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_praktikum_state1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_praktikum_state1/flutter_praktikum_state1_multi/android/app/src/main/kotlin/com/example/flutter_praktikum_state1_multi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_praktikum_state1_multi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
